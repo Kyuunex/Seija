7a3ca0cef52929fd6d5dfcf9938843b947487894
Kyuunex <kyuunex@protonmail.ch>
2020-02-19 04:00:58 +0400

    update modules to use aiosqlite

--- modules/first_run.py
+++ modules/first_run.py
@@ -1,22 +1,30 @@
-from modules import db
+import sqlite3
 from modules.connections import database_file as database_file
 import os
 
 
 async def add_admins(self):
-    if not db.query("SELECT * FROM admins"):
+    async with await self.db.execute("SELECT * FROM admins") as cursor:
+        admin_list = await cursor.fetchall()
+
+    if not admin_list:
         app_info = await self.application_info()
         if app_info.team:
             for team_member in app_info.team.members:
-                db.query(["INSERT INTO admins VALUES (?, ?)", [str(team_member.id), "1"]])
+                await self.db.execute("INSERT INTO admins VALUES (?, ?)", [str(team_member.id), "1"])
                 print(f"Added {team_member.name} to admin list")
         else:
-            db.query(["INSERT INTO admins VALUES (?, ?)", [str(app_info.owner.id), "1"]])
+            await self.db.execute("INSERT INTO admins VALUES (?, ?)", [str(app_info.owner.id), "1"])
             print(f"Added {app_info.owner.name} to admin list")
+        await self.db.commit()
 
 
 def create_tables():
     if not os.path.exists(database_file):
+        conn = sqlite3.connect(database_file)
+        c = conn.cursor()
+        c.execute("CREATE TABLE config (setting, parent, value, flag)")
+        c.execute("CREATE TABLE admins (user_id, permissions)")
         db.query("CREATE TABLE users "
                  "(user_id, osu_id, osu_username, osu_join_date, pp, country, ranked_maps_amount, no_sync)")
         db.query("CREATE TABLE user_event_history (osu_id, event_id, channel_id, timestamp)")
@@ -41,3 +49,5 @@ def create_tables():
         db.query("CREATE TABLE member_goodbye_messages (message)")
         db.query(["INSERT INTO member_goodbye_messages VALUES (?)", ["%s is going for loved"]])
         db.query(["INSERT INTO member_goodbye_messages VALUES (?)", ["%s was told to remap one too many times"]])
+        conn.commit()
+        conn.close()

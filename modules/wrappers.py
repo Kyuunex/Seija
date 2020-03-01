33b7848237929f830b70a672a59d226026db4abc
Kyuunex <kyuunex@protonmail.ch>
2020-03-01 09:16:18 +0400

    update wrappers.py

--- modules/wrappers.py
+++ modules/wrappers.py
@@ -23,11 +23,19 @@ async def send_large_embed(channel, embed, contents):
         embed.description = output
         await channel.send(embed=embed)
 
+
 def in_db_list(a_list, what):
     for item in a_list:
         if what == item[0]:
             return True
     return False
+
+
+def unnest_list(a_list):
+    buffer_list = []
+    for item in a_list:
+        buffer_list.append(item[0])
+    return buffer_list
 
 
 def get_member_guaranteed(ctx, lookup):

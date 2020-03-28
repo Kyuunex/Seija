91e48d963c938402a12e85551c2cb845fae7841e
Kyuunex <kyuunex@protonmail.ch>
2020-03-28 07:53:14 +0400

    fix update_user_discord_account

--- cogs/MemberVerification.py
+++ cogs/MemberVerification.py
@@ -52,11 +52,11 @@ class MemberVerification(commands.Cog):
     @commands.command(name="update_user_discord_account", brief="When user switched accounts, apply this")
     @commands.check(permissions.is_admin)
     async def update_user_discord_account(self, ctx, old_id, new_id):
-        await self.bot.db.execute("UPDATE users SET user_id = ? WHERE user_id = ?", [str(old_id), str(new_id)])
-        await self.bot.db.execute("UPDATE map_owners SET user_id = ? WHERE user_id = ?", [str(old_id), str(new_id)])
-        await self.bot.db.execute("UPDATE queues SET user_id = ? WHERE user_id = ?", [str(old_id), str(new_id)])
+        await self.bot.db.execute("UPDATE users SET user_id = ? WHERE user_id = ?", [str(new_id), str(old_id)])
+        await self.bot.db.execute("UPDATE map_owners SET user_id = ? WHERE user_id = ?", [str(new_id), str(old_id)])
+        await self.bot.db.execute("UPDATE queues SET user_id = ? WHERE user_id = ?", [str(new_id), str(old_id)])
         await self.bot.db.execute("UPDATE mapset_channels SET user_id = ? WHERE user_id = ?",
-                                  [str(old_id), str(new_id)])
+                                  [str(new_id), str(old_id)])
         await self.bot.db.commit()
         await ctx.send("lol ok")
 

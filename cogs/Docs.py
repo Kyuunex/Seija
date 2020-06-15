05669cd8793795b28e749f0c86bca7a5aaf0885a
Kyuunex <kyuunex@protonmail.ch>
2020-06-15 16:45:06 +0400

    refactored Docs.py

--- cogs/Docs.py
+++ cogs/Docs.py
@@ -4,51 +4,67 @@ from modules import permissions
 
 
 class Docs(commands.Cog):
-    help_thumbnail = "https://i.imgur.com/JhL9PV8.png"
-    author_icon = "https://i.imgur.com/1icHC5a.png"
-    author_text = "Seija"
-    footer_icon = "https://avatars0.githubusercontent.com/u/5400432"
-    footer_text = "Made by Kyuunex"
-
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="docs", brief="Pretty help command", description="")
+    @commands.command(name="docs", brief="A friendlier looking help command.")
     @commands.check(permissions.is_not_ignored)
     async def docs(self, ctx, sub_help=None):
+        """
+        A custom help command written to look more friendly to end users.
+        """
+
         if sub_help == "veto":
-            async with self.bot.db.execute("SELECT channel_id FROM channels WHERE setting = ? AND channel_id = ?",
+            async with self.bot.db.execute("SELECT channel_id FROM channels "
+                                           "WHERE setting = ? AND channel_id = ?",
                                            ["veto", str(ctx.channel.id)]) as cursor:
-                is_veto_channel = await cursor.fetchall()
-            if is_veto_channel:
-                await ctx.send(embed=await self.veto())
+                is_veto_channel = await cursor.fetchone()
+            if not is_veto_channel:
+                embed = await self.main(ctx)
+            else:
+                embed = await self.veto()
         elif sub_help == "mapset_channel":
-            await ctx.send(embed=await self.mapset_channel())
+            embed = await Docs.mapset_channel()
         elif sub_help == "queue":
-            await ctx.send(embed=await self.queue(ctx.author))
+            embed = await Docs.queue(ctx.author)
         elif sub_help == "mapset_channel_management":
-            await ctx.send(embed=await Docs.mapset_channel_management())
+            embed = await Docs.mapset_channel_management()
         elif sub_help == "queue_management":
-            await ctx.send(embed=await Docs.queue_management())
+            embed = await Docs.queue_management()
         else:
-            await ctx.send(embed=await self.main(ctx))
+            embed = await self.main(ctx)
+
+        embed.set_thumbnail(url="https://i.imgur.com/JhL9PV8.png")
+        # embed.set_author(name="Seija", icon_url="https://i.imgur.com/1icHC5a.png")
+        embed.set_footer(text="Made by Kyuunex", icon_url="https://avatars0.githubusercontent.com/u/5400432")
+
+        await ctx.send(embed=embed)
 
     async def main(self, ctx):
         embed = discord.Embed(title="Seija teaches you how to be a bot master.",
-                              description="Any abuse will be dealt with punishment.", color=0xbd3661)
-        embed.add_field(name=".docs mapset_channel", value="Show a help menu for requesting a mapset channel.",
+                              description="Any abuse will be dealt with punishment.",
+                              color=0xbd3661)
+        embed.add_field(name=".docs mapset_channel",
+                        value="Show a help menu for requesting a mapset channel.",
                         inline=False)
-        embed.add_field(name=".docs queue", value="Show a help menu for requesting a queue channel.",
+        embed.add_field(name=".docs queue",
+                        value="Show a help menu for requesting a queue channel.",
                         inline=False)
-        embed.add_field(name=".docs mapset_channel_management", value="Show mapset channel management commands.",
+        embed.add_field(name=".docs mapset_channel_management",
+                        value="Show mapset channel management commands.",
                         inline=False)
-        embed.add_field(name=".docs queue_management", value="Show queue channel management commands.",
+        embed.add_field(name=".docs queue_management",
+                        value="Show queue channel management commands.",
                         inline=False)
+
         async with self.bot.db.execute("SELECT channel_id FROM channels WHERE setting = ? AND channel_id = ?",
                                        ["veto", str(ctx.channel.id)]) as cursor:
-            is_veto_channel = await cursor.fetchall()
+            is_veto_channel = await cursor.fetchone()
         if is_veto_channel:
-            embed.add_field(name=".docs veto", value="Commands for tracking a mapset in veto mode.", inline=True)
+            embed.add_field(name=".docs veto",
+                            value="Commands for tracking a mapset in veto mode.",
+                            inline=True)
+
         embed.add_field(name=".from (country_name)",
                         value="Retrieve a list of server members who are from the specified country. "
                               "Takes Alpha-2, Alpha-3 codes and full country names.",
@@ -57,71 +73,79 @@ class Docs(commands.Cog):
                         value="Show server demographics. "
                               "Shows how many members are from which country and the percentage of them.",
                         inline=False)
-        embed.add_field(name=".ts (mod)", value="Send a clickable timestamp for the osu! editor. "
-                                                "The message must start with a timestamp.",
+        embed.add_field(name=".ts (mod)",
+                        value="Send a clickable timestamp for the osu! editor. "
+                              "The message must start with a timestamp.",
                         inline=False)
-        embed.set_thumbnail(url=Docs.help_thumbnail)
-        embed.set_author(name=Docs.author_text, icon_url=Docs.author_icon)
-        embed.set_footer(text=Docs.footer_text, icon_url=Docs.footer_icon)
         return embed
 
-    async def veto(self):
-        embed = discord.Embed(title="~~BNS PLS MUTUAL ME~~", description="**Veto tracking commands:**",
+    @staticmethod
+    async def veto():
+        embed = discord.Embed(title="~~BNS PLS MUTUAL ME~~",
+                              description="**Veto tracking commands:**",
                               color=0xbd3661)
-        embed.add_field(name=".veto <mapset_id>", value="Track a mapset in this channel in veto mode.", inline=False)
+        embed.add_field(name=".veto <mapset_id>",
+                        value="Track a mapset in this channel in veto mode.",
+                        inline=False)
         embed.add_field(name=".unveto <mapset_id>",
-                        value="Untrack a mapset in this channel in veto mode.", inline=False)
-        embed.set_thumbnail(url=Docs.help_thumbnail)
-        embed.set_author(name=Docs.author_text, icon_url=Docs.author_icon)
-        embed.set_footer(text=Docs.footer_text, icon_url=Docs.footer_icon)
+                        value="Untrack a mapset in this channel in veto mode.",
+                        inline=False)
+
         return embed
 
-    async def queue(self, author):
+    @staticmethod
+    async def queue(author):
         channel_friendly_author = author.display_name.replace(" ", "_").lower()
-        text = "**__Queue creation command:__**"
-        text += "\n`.request_queue (queue type)` - Create a modding queue. By default, the queue will be closed."
-        text += "\n`(queue type)` is an optional argument that specifies " \
-                "what goes between your username and the word `queue` in the title of the channel. " \
-                "If no argument is supplied, `std` will be filled automatically. Please follow our naming standards."
+
+        text = "**__Queue creation command:__**\n"
+        text += "`.request_queue (queue type)` - Create a modding queue. \n"
+        text += "By default, the queue will be closed.\n"
+        text += "`(queue type)` is an optional argument that specifies "
+        text += "what goes between your username and the word `queue` in the title of the channel. \n"
+        text += "If no argument is supplied, `std` will be filled automatically. \n"
+        text += "Please follow our naming standards.\n"
         text += "\n"
-        text += "\n**__Examples:__**"
-        text += f"\n`.request_queue` - This example will create `#{channel_friendly_author}-std-queue`"
-        text += f"\n`.request_queue mania` - This example will create `#{channel_friendly_author}-mania-queue`"
-        text += f"\n`.request_queue taiko-bn` - This example will create `#{channel_friendly_author}-taiko-bn-queue`"
+        text += "**__Examples:__**\n"
+        text += f"`.request_queue` - This example will create `#{channel_friendly_author}-std-queue`\n"
+        text += f"`.request_queue mania` - This example will create `#{channel_friendly_author}-mania-queue`\n"
+        text += f"`.request_queue taiko-bn` - This example will create `#{channel_friendly_author}-taiko-bn-queue`\n"
         text += "\n"
-        text += "\nFor queue management commands, type `.docs queue_management`"
+        text += "For queue management commands, type `.docs queue_management`\n"
         text += "\n"
-        text += "\n**Remember:**"
-        text += "\n**__Do not__ create a queue __only__ for GD requests.**"
-        text += "\n**__It should__ be a modding queue, not a diary and not an image dump. " \
-                "You can be creative and do other things but its primary purpose must be a modding queue.**"
-        embed = discord.Embed(title="With this command you can create a queue channel.", description=text,
+        text += "**Remember:**\n"
+        text += "**__Do not__ create a queue __only__ for GD requests.**\n"
+        text += "**__It should__ be a modding queue, not a diary and not an image dump. \n"
+        text += "You can be creative and do other things but its primary purpose must be a modding queue.**\n"
+
+        embed = discord.Embed(title="With this command you can create a queue channel.",
+                              description=text,
                               color=0xbd3661)
-        embed.set_author(name=Docs.author_text, icon_url=Docs.author_icon)
-        embed.set_footer(text=Docs.footer_text, icon_url=Docs.footer_icon)
+
         return embed
 
-    async def mapset_channel(self):
-        text = "**__Mapset channel creation command:__**: "
-        text += "\n`.request_mapset_channel (mapset id) (song name)` - " \
-                "This is the general command to create a mapset channel."
-        text += "\n`(song name)` is an optional argument that is not required. "
-        text += "\nIf the mapset is not uploaded yet, `(mapset id)` can be set to `0` " \
-                "but in that case, the `(song name)` argument is required."
+    @staticmethod
+    async def mapset_channel():
+        text = "**__Mapset channel creation command:__**: \n"
+        text += "`.request_mapset_channel (mapset id) (song name)` - "
+        text += "This is the general command to create a mapset channel.\n"
+        text += "`(song name)` is an optional argument that is not required. \n"
+        text += "If the mapset is not uploaded yet, `(mapset id)` can be set to `0` "
+        text += "but in that case, the `(song name)` argument is required.\n"
         text += "\n"
-        text += "\n**__Examples:__**"
-        text += "\n`.request_mapset_channel 817793` - An example usage with mapset ID."
-        text += "\n`.request_mapset_channel 0 Futanari Nari ni` - An example usage with just the song title."
+        text += "**__Examples:__**\n"
+        text += "`.request_mapset_channel 817793` - An example usage with mapset ID.\n"
+        text += "`.request_mapset_channel 0 Futanari Nari ni` - An example usage with just the song title.\n"
         text += "\n"
-        text += "\nFor mapset channel management commands, type `.docs mapset_channel_management`"
+        text += "For mapset channel management commands, type `.docs mapset_channel_management`\n"
         text += "\n"
-        text += "\n**Remember:**"
-        text += "\n**__Do not__ create a mapset channel for single person sets. **"
-        text += "\n**__Do not__ create a channel if you don't have GDers/collaborators ready.**"
+        text += "**Remember:**\n"
+        text += "**__Do not__ create a mapset channel for single person sets. **\n"
+        text += "**__Do not__ create a channel if you don't have GDers/collaborators ready.**\n"
+
         embed = discord.Embed(title="With this command you can create a mapset channel for collaborative purposes.",
-                              description=text, color=0xbd3661)
-        embed.set_author(name=Docs.author_text, icon_url=Docs.author_icon)
-        embed.set_footer(text=Docs.footer_text, icon_url=Docs.footer_icon)
+                              description=text,
+                              color=0xbd3661)
+
         return embed
 
     @staticmethod
@@ -131,7 +155,9 @@ class Docs(commands.Cog):
                                           "unless you want to ban a specific person or a role from your queue "
                                           "or the bot is down.**",
                               color=0xbd3661)
-        embed.add_field(name=".open", value="Open the queue, everyone can see it and post in it.", inline=False)
+        embed.add_field(name=".open",
+                        value="Open the queue, everyone can see it and post in it.",
+                        inline=False)
         embed.add_field(name=".close",
                         value="Close the queue, everyone can see it but can't post in it. "
                               "You can also use this command to unhide the queue, "
@@ -168,8 +194,7 @@ class Docs(commands.Cog):
                         value="Give your creator permissions of the queue to someone. "
                               "This will clear all co-owners too.",
                         inline=False)
-        embed.set_author(name=Docs.author_text, icon_url=Docs.author_icon)
-        embed.set_footer(text=Docs.footer_text, icon_url=Docs.footer_icon)
+
         return embed
 
     @staticmethod
@@ -201,8 +226,7 @@ class Docs(commands.Cog):
                               "or `notification` for the notification type.",
                         inline=False)
         embed.add_field(name=".untrack", value="Untrack everything in this channel.", inline=False)
-        embed.set_author(name=Docs.author_text, icon_url=Docs.author_icon)
-        embed.set_footer(text=Docs.footer_text, icon_url=Docs.footer_icon)
+
         return embed
 
 

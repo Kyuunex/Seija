c2ee3ecda69ee77428c9078477a017e1a33af7d1
Kyuunex <kyuunex@protonmail.ch>
2020-07-01 11:27:21 +0400

    strip() instead of rstrip() in connections.py

--- modules/connections.py
+++ modules/connections.py
@@ -1,3 +1,3 @@
-osu_api_key = (open("data/osu_api_key.txt", "r+").read()).rstrip()
-bot_token = (open("data/token.txt", "r+").read()).rstrip()
+osu_api_key = (open("data/osu_api_key.txt", "r+").read()).strip()
+bot_token = (open("data/token.txt", "r+").read()).strip()
 database_file = "data/maindb.sqlite3"

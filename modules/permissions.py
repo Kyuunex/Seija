6c8681d0591bcf7412aa146fdcb624d5b4c5eda5
Kyuunex <kyuunex@protonmail.ch>
2020-08-09 10:34:31 +0400

    fix permissions.py by type casting

--- modules/permissions.py
+++ modules/permissions.py
@@ -11,41 +11,41 @@ conn.close()
 
 admin_list = []
 for admin_id in db_admin_list:
-    admin_list.append(admin_id[0])
+    admin_list.append(int(admin_id[0]))
 
 
 owner_list = []
 for owner_id in db_owner_list:
-    owner_list.append(owner_id[0])
+    owner_list.append(int(owner_id[0]))
 
 
 ignored_users = []
 for ignored_user in db_ignored_users:
-    ignored_users.append(ignored_user[0])
+    ignored_users.append(int(ignored_user[0]))
 
 
 async def is_admin(ctx):
-    return str(ctx.author.id) in admin_list
+    return int(ctx.author.id) in admin_list
 
 
 async def is_owner(ctx):
-    return str(ctx.author.id) in owner_list
+    return int(ctx.author.id) in owner_list
 
 
 async def is_not_ignored(ctx):
-    return not (str(ctx.author.id) in ignored_users)
+    return not (int(ctx.author.id) in ignored_users)
 
 
 async def is_ignored(ctx):
-    return str(ctx.author.id) in ignored_users
+    return int(ctx.author.id) in ignored_users
 
 
 def check_admin(user_id):
-    return str(user_id) in admin_list
+    return int(user_id) in admin_list
 
 
 def check_owner(user_id):
-    return str(user_id) in owner_list
+    return int(user_id) in owner_list
 
 
 async def channel_ban_members(ctx):

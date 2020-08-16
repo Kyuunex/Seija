9f4280a9ddbcd4cded2b12b1233fc4e87213210e
Kyuunex <kyuunex@protonmail.ch>
2020-08-16 16:41:50 +0400

    in permissions.py, move stuff that fetches users into a function

--- modules/permissions.py
+++ modules/permissions.py
@@ -1,27 +1,31 @@
 from modules.connections import database_file as database_file
 import sqlite3
 
-conn = sqlite3.connect(database_file)
-c = conn.cursor()
-db_admin_list = tuple(c.execute("SELECT user_id FROM admins"))
-db_owner_list = tuple(c.execute("SELECT user_id FROM admins WHERE permissions = ?", [str(1)]))
-db_ignored_users = tuple(c.execute("SELECT user_id FROM ignored_users"))
-conn.commit()
-conn.close()
-
 admin_list = []
-for admin_id in db_admin_list:
-    admin_list.append(int(admin_id[0]))
-
-
 owner_list = []
-for owner_id in db_owner_list:
-    owner_list.append(int(owner_id[0]))
-
-
 ignored_users = []
-for ignored_user in db_ignored_users:
-    ignored_users.append(int(ignored_user[0]))
+
+
+def fetch_users_from_db():
+    conn = sqlite3.connect(database_file)
+    c = conn.cursor()
+    db_admin_list = tuple(c.execute("SELECT user_id FROM admins"))
+    db_owner_list = tuple(c.execute("SELECT user_id FROM admins WHERE permissions = ?", [str(1)]))
+    db_ignored_users = tuple(c.execute("SELECT user_id FROM ignored_users"))
+    conn.commit()
+    conn.close()
+
+    for admin_id in db_admin_list:
+        admin_list.append(int(admin_id[0]))
+
+    for owner_id in db_owner_list:
+        owner_list.append(int(owner_id[0]))
+
+    for ignored_user in db_ignored_users:
+        ignored_users.append(int(ignored_user[0]))
+
+
+fetch_users_from_db()
 
 
 async def is_admin(ctx):

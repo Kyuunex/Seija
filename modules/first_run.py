8af7383c27698ab9b83ea3d14f7c7f896d9fbb16
Kyuunex <kyuunex@protonmail.ch>
2020-09-13 22:27:29 +0400

    made some stuff in db not nullable and unique

--- modules/first_run.py
+++ modules/first_run.py
@@ -23,38 +23,133 @@ def create_tables():
     if not os.path.exists(database_file):
         conn = sqlite3.connect(database_file)
         c = conn.cursor()
-        c.execute("""CREATE TABLE "config" ("setting" TEXT, "parent" TEXT, "value" TEXT, "flag" TEXT)""")
-        c.execute("""CREATE TABLE "admins" ("user_id" INTEGER, "permissions" INTEGER)""")
-        c.execute("""CREATE TABLE "ignored_users" ("user_id" INTEGER, "reason" TEXT)""")
+        c.execute("""
+        CREATE TABLE "config" (
+        "setting"    TEXT, 
+        "parent"    TEXT,
+        "value"    TEXT,
+        "flag"    TEXT
+        )
+        """)
+        c.execute("""
+        CREATE TABLE "admins" (
+            "user_id"    INTEGER NOT NULL UNIQUE,
+            "permissions"    INTEGER NOT NULL
+        )
+        """)
+        c.execute("""
+        CREATE TABLE "ignored_users" (
+            "user_id"    INTEGER NOT NULL UNIQUE,
+            "reason"    TEXT
+        )
+        """)
 
-        c.execute("""CREATE TABLE "users" ("user_id" INTEGER, "osu_id" INTEGER, "osu_username" TEXT, 
-        "osu_join_date" INTEGER, "pp" INTEGER, "country" TEXT, 
-        "ranked_maps_amount" INTEGER, "kudosu" INTEGER, "no_sync" INTEGER)""")
+        c.execute("""
+        CREATE TABLE "users" (
+            "user_id"    INTEGER NOT NULL UNIQUE,
+            "osu_id"    INTEGER NOT NULL,
+            "osu_username"    TEXT NOT NULL,
+            "osu_join_date"    INTEGER,
+            "pp"    INTEGER,
+            "country"    TEXT,
+            "ranked_maps_amount"    INTEGER,
+            "kudosu"    INTEGER,
+            "no_sync"    INTEGER
+        )
+        """)
 
-        c.execute("""CREATE TABLE "channels" ("setting" TEXT, "guild_id" INTEGER, "channel_id" INTEGER)""")
-        c.execute("""CREATE TABLE "categories" ("setting" TEXT, "guild_id" INTEGER, "category_id" INTEGER)""")
-        c.execute("""CREATE TABLE "roles" ("setting" TEXT, "guild_id" INTEGER, "role_id" INTEGER)""")
+        c.execute("""
+        CREATE TABLE "channels" (
+            "setting"    TEXT NOT NULL,
+            "guild_id"    INTEGER NOT NULL,
+            "channel_id"    INTEGER NOT NULL
+        )
+        """)
+        c.execute("""
+        CREATE TABLE "categories" (
+            "setting"    TEXT NOT NULL,
+            "guild_id"    INTEGER NOT NULL,
+            "category_id"    INTEGER NOT NULL
+        )
+        """)
+        c.execute("""
+        CREATE TABLE "roles" (
+            "setting"    TEXT NOT NULL,
+            "guild_id"    INTEGER NOT NULL,
+            "role_id"    INTEGER NOT NULL
+        )
+        """)
 
-        c.execute("""CREATE TABLE "mod_post_history" ("post_id" INTEGER, "mapset_id" INTEGER, "channel_id" INTEGER)""")
+        c.execute("""
+        CREATE TABLE "mod_post_history" (
+            "post_id"    INTEGER NOT NULL,
+            "mapset_id"    INTEGER NOT NULL,
+            "channel_id"    INTEGER NOT NULL
+        )
+        """)
 
-        c.execute("""CREATE TABLE "mapset_nomination_history" ("event_id" INTEGER, "mapset_id" INTEGER, 
-        "channel_id" INTEGER)""")
+        c.execute("""
+        CREATE TABLE "mapset_nomination_history" (
+            "event_id"    INTEGER NOT NULL,
+            "mapset_id"    INTEGER NOT NULL,
+            "channel_id"    INTEGER NOT NULL
+        )
+        """)
 
-        c.execute("""CREATE TABLE "mod_tracking" ("mapset_id" INTEGER, "channel_id" INTEGER, "mode" INTEGER)""")
+        c.execute("""
+        CREATE TABLE "mod_tracking" (
+            "mapset_id"    INTEGER NOT NULL,
+            "channel_id"    INTEGER NOT NULL,
+            "mode"    INTEGER NOT NULL
+        )
+        """)
 
-        c.execute("""CREATE TABLE "mapset_notification_status" ("mapset_id" INTEGER, "map_id" INTEGER, 
-        "channel_id" INTEGER, "status" INTEGER)""")
+        c.execute("""
+        CREATE TABLE "mapset_notification_status" (
+            "mapset_id"    INTEGER NOT NULL,
+            "map_id"    INTEGER,
+            "channel_id"    INTEGER NOT NULL,
+            "status"    INTEGER NOT NULL
+        )
+        """)
 
-        c.execute("""CREATE TABLE "difficulty_claims" ("map_id" INTEGER, "user_id" INTEGER)""")
-        c.execute("""CREATE TABLE "restricted_users" ("guild_id" INTEGER, "osu_id" INTEGER)""")
+        c.execute("""
+        CREATE TABLE "difficulty_claims" (
+            "map_id"    INTEGER NOT NULL,
+            "user_id"    INTEGER NOT NULL
+        )
+        """)
+        c.execute("""
+        CREATE TABLE "restricted_users" (
+            "guild_id"    INTEGER NOT NULL,
+            "osu_id"    INTEGER NOT NULL
+        )
+        """)
 
-        c.execute("""CREATE TABLE "queues" ( "channel_id" INTEGER, "user_id" INTEGER, 
-        "guild_id" INTEGER, "is_creator" INTEGER)""")
+        c.execute("""
+        CREATE TABLE "queues" (
+            "channel_id"    INTEGER NOT NULL,
+            "user_id"    INTEGER NOT NULL,
+            "guild_id"    INTEGER NOT NULL,
+            "is_creator"    INTEGER NOT NULL
+        )
+        """)
 
-        c.execute("""CREATE TABLE "mapset_channels" ("channel_id" INTEGER, "role_id" INTEGER, 
-        "user_id" INTEGER, "mapset_id" INTEGER, "guild_id" INTEGER)""")
+        c.execute("""
+        CREATE TABLE "mapset_channels" (
+            "channel_id"    INTEGER NOT NULL UNIQUE,
+            "role_id"    INTEGER NOT NULL UNIQUE,
+            "user_id"    INTEGER NOT NULL,
+            "mapset_id"    INTEGER,
+            "guild_id"    INTEGER NOT NULL
+        )
+        """)
 
-        c.execute("""CREATE TABLE "member_goodbye_messages" ("message" TEXT)""")
+        c.execute("""
+        CREATE TABLE "member_goodbye_messages" (
+            "message"    TEXT NOT NULL
+        )
+        """)
 
         c.execute("INSERT INTO member_goodbye_messages VALUES (?)", ["%s is going for loved"])
         c.execute("INSERT INTO member_goodbye_messages VALUES (?)", ["%s was told to remap one too many times"])

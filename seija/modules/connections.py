813b5b238bd6708de25b34fe54b24da1c09e7c33
Kyuunex <kyuunex@protonmail.ch>
2021-07-17 03:02:12 +0400

    update storage management system

--- seija/modules/connections.py
+++ seija/modules/connections.py
@@ -1,12 +1,12 @@
 import os
-from seija.modules.storage_management import dirs
+from seija.modules.storage_management import BOT_DATA_DIR
 
 
 if os.environ.get('SEIJA_TOKEN'):
     bot_token = os.environ.get('SEIJA_TOKEN')
 else:
     try:
-        with open(dirs.user_data_dir + "/token.txt", "r+") as token_file:
+        with open(BOT_DATA_DIR + "/token.txt", "r+") as token_file:
             bot_token = token_file.read().strip()
     except FileNotFoundError as e:
         print("i need a bot token. either set SEIJA_TOKEN environment variable")
@@ -17,7 +17,7 @@ if os.environ.get('SEIJA_OSU_API_KEY'):
     osu_api_key = os.environ.get('SEIJA_OSU_API_KEY')
 else:
     try:
-        with open(dirs.user_data_dir + "/osu_api_key.txt", "r+") as token_file:
+        with open(BOT_DATA_DIR + "/osu_api_key.txt", "r+") as token_file:
             osu_api_key = token_file.read().strip()
     except FileNotFoundError as e:
         print("i need a osu api key. either set SEIJA_OSU_API_KEY environment variable")
@@ -28,7 +28,7 @@ if os.environ.get('SEIJA_CLIENT_ID'):
     client_id = os.environ.get('SEIJA_CLIENT_ID')
 else:
     try:
-        with open(dirs.user_data_dir + "/client_id.txt", "r+") as token_file:
+        with open(BOT_DATA_DIR + "/client_id.txt", "r+") as token_file:
             client_id = token_file.read().strip()
     except FileNotFoundError as e:
         print("i need a osu web client id. either set SEIJA_CLIENT_ID environment variable")
@@ -39,7 +39,7 @@ if os.environ.get('SEIJA_CLIENT_SECRET'):
     client_secret = os.environ.get('SEIJA_CLIENT_SECRET')
 else:
     try:
-        with open(dirs.user_data_dir + "/client_secret.txt", "r+") as token_file:
+        with open(BOT_DATA_DIR + "/client_secret.txt", "r+") as token_file:
             client_secret = token_file.read().strip()
     except FileNotFoundError as e:
         print("i need a osu web client secret. either set SEIJA_CLIENT_SECRET environment variable")

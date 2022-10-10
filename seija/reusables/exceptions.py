b318a3e3e81646f8e9c207470fdcf45934a9dbf0
Kyuunex <kyuunex@protonmail.ch>
2022-10-10 18:45:32 +0400

    improve error messages

--- seija/reusables/exceptions.py
+++ seija/reusables/exceptions.py
@@ -3,8 +3,8 @@ from discord.utils import escape_markdown
 
 
 async def embed_exception(exception):
-    embed = discord.Embed(title="Exception",
+    embed = discord.Embed(title="An error has occurred",
                           description=escape_markdown(str(exception)),
-                          color=0xff0000)
+                          color=0xff6666)
     embed.set_footer(text="This error information is for staff, just ignore it.")
     return embed

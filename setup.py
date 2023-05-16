2411040004b56b44bfc0ee07753c3539cd6ef0eb
Kyuunex <kyuunex@protonmail.ch>
2023-05-16 22:19:54 +0400

    bump discord.py req

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author_email='kyuunex@protonmail.ch',
     url='https://github.com/Kyuunex/Seija',
     install_requires=[
-        'discord.py[voice]==2.2.2',
+        'discord.py[voice]==2.2.3',
         'pycountry',
         'aiosqlite',
         'psutil',
